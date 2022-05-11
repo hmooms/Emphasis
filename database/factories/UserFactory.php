484e283c8520ba8c1c892f2b328248bdf6a1ffbc
Hugo Ooms <hmooms@hotmail.com>
2022-05-11 17:43:59 +0200

    factory dingen verwijderd

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,38 +34,4 @@ class UserFactory extends Factory
             'isAdmin' => false,
         ];
     }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
-
-    /**
-     * Indicate that the user should have a personal team.
-     *
-     * @return $this
-     */
-    public function withPersonalTeam()
-    {
-        if (! Features::hasTeamFeatures()) {
-            return $this->state([]);
-        }
-
-        return $this->has(
-            Team::factory()
-                ->state(function (array $attributes, User $user) {
-                    return ['name' => $user->name.'\'s Team', 'user_id' => $user->id, 'personal_team' => true];
-                }),
-            'ownedTeams'
-        );
-    }
 }
