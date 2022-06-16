ef78aea32bd1a5fad2abbc57c7edb7eb665e1377
Hugo Ooms <hmooms@hotmail.com>
2022-06-16 14:20:47 +0200

    made project factory and added to databse seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\Project;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +19,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
         
+        Project::factory(5)->create();
+
         DB::table('users')->insert([
             'name' => 'Admin',
             'email' => 'admin@account.com',
@@ -27,5 +32,19 @@ class DatabaseSeeder extends Seeder
             'remember_token' => Str::random(10),
             'is_admin' => true,
         ]);
+
+        $users = User::where('is_admin', false)->get();
+
+
+        // itterate over each project and add a rondom user and add all admin
+        Project::all()->each(function($project) use ($users) {
+            $project->users()->attach(
+                $users->random(rand(1, 3))->pluck('id')->toArray()
+            );
+            $project->users()->attach(
+                User::where('is_admin', true)->get()->pluck('id')->toArray()
+            );
+        });
+
     }
 }
