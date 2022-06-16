2866494c4cfc8949ce9bdfc5d22f9fa893d809f6
Hugo Ooms <hmooms@hotmail.com>
2022-06-16 12:54:16 +0200

    updated project user table

--- database/migrations/2022_06_16_103241_create_project_user_table.php
+++ database/migrations/2022_06_16_103241_create_project_user_table.php
@@ -15,6 +15,10 @@ class CreateProjectUserTable extends Migration
     {
         Schema::create('project_user', function (Blueprint $table) {
             $table->id();
+            $table->integer('project_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            
             $table->foreign('project_id')->references('id')->on('projects');
             $table->foreign('user_id')->references('id')->on('users');
         });
