5318059e5bffee38c8b1e18a1e66d3a278f62aec
Hugo Ooms <hmooms@hotmail.com>
2022-06-22 02:18:21 +0200

    added click event to go to project information made timeremaingincalculator js

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,47 +1,16 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import { Inertia } from '@inertiajs/inertia';
 import { Link } from '@inertiajs/inertia-vue3';
+import { calculateRemainingTime, isOverdue } from './Project/Partials/TimeRemainingCalculator.js';
 
 defineProps({
     projects: Array,
 })
 
-// amount of miliseconds
-const intervals = {
-    hour: 3600000,
-    day: 86400000,
-    week: 604800000,
-    month: 18144000000
+const showProject = (id) => {
+    Inertia.get(route('project.show', id));
 }
-
-const currentTime = Date.now();
-
-const calculateRemainingTime = (dueTime) => {
-    return calcuateInterval(Date.parse(dueTime) - currentTime);
-}
-
-// check if it should be displayed in month, week, day or hours
-const calcuateInterval = (timeDifference) => {
-    if (timeDifference >= intervals.month || timeDifference <= -intervals.month) {
-        let months = Math.floor(timeDifference / intervals.month)
-        return (months > 1 || months < -1) ? `${months} maanden` : `${months} maand`;
-    } else if (timeDifference >= intervals.week || timeDifference <= -intervals.week) {
-        let weeks = Math.floor(timeDifference / intervals.week)
-        return (weeks > 1 || weeks < -1) ? `${weeks} weeken` : `${weeks} week`;
-    } else if (timeDifference >= intervals.day || timeDifference <= -intervals.day) {
-        let days = Math.floor(timeDifference / intervals.day)
-        let hours = Math.floor((timeDifference - intervals.day * days) / intervals.hour)
-        return (days > 1 || days < -1) ? `${days} dagen ${hours} uur` : `${days} dag ${hours} uur`;
-    } else if (timeDifference >= intervals.hour || timeDifference <= intervals.hour) {
-        let hours = Math.floor(timeDifference / intervals.hour)
-        return `${hours} uur`;
-    }
-}
-
-const isOverdue = (dueTime) => {
-    return Date.parse(dueTime) < currentTime;
-}
-
 </script>
 
 <template>
@@ -64,7 +33,8 @@ const isOverdue = (dueTime) => {
             <div v-if="projects" v-for="project in projects">
 
                 <div :class="[(isOverdue(project.end_date) && !project.is_completed) ? 'border-danger' : null], [project.is_completed ? 'border-success' : null]"
-                    class="p-4 mb-1 border bg-white-bg flex justify-between">
+                    class="p-4 mb-1 border bg-white-bg flex justify-between hover:bg-gray-300 hover:cursor-pointer"
+                    @click="showProject(project.id)">
 
                     <p class="text-dark-font">{{ project.title }}</p>
                     <div v-if="!project.is_completed">
